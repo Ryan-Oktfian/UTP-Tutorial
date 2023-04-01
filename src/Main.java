df5869d6a7c6faaabf5c0f6f0e8d7f6fa2da81c8
Ryan-Oktfian <ryanoktfian@gmail.com>
2023-04-01 08:48:36 +0700

    Submit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    System.out.println("cek cek 1 2 3");
+    System.out.println("dicoba dicoba");
   }
 }
